fa8f457ce10657a072c172f5ebb9952c61fe7ed5
Ivan-Tarasenko <80888936+Ivan-Tarasenko@users.noreply.github.com>
2022-09-29 21:08:53 +1100

    Initial Commit

---
+++ TODO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TODO
+//
+//  Created by Иван Тарасенко on 29.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
