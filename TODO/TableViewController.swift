5f7802946e61be6ad57dfe1be97cbfefc4636629
Ivan-Tarasenko <80888936+Ivan-Tarasenko@users.noreply.github.com>
2022-11-06 20:52:18 +0300

    add swift lint, license, tableViewController

---
+++ TODO/TableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewController.swift
+//  TODO
+//
+//  Created by Иван Тарасенко on 06.11.2022.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+}
